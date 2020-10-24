d1424a27268f1c43cd1b20e360a271cc9a10c652
Wenlin Huang <jackhwl@hotmail.com>
2020-10-24 12:07:12 -0400

    Challenge 5: Solution stack

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,43 @@ interface IStack<T> {
   print(): void;
 }
 
-// export class Stack<T> {
+interface IStackNode<T> {
+  data: T;
+  next: IStackNode<T>;
+}
 
-// }
+export class Stack<T> implements IStack<T> {
+  private head: IStackNode<T> | undefined;
+  push(items: T | T[]): IStack<T> {
+    if (Array.isArray(items)) {
+      items.forEach(item => this.push(item));
+    } else {
+      const n: IStackNode<T> = {data: items, next: this.head as IStackNode<T>};
+      this.head = n;
+    }
+    return this;
+  }
+  pop() : T | undefined {
+    const n = this.head;
+    if (!n) return undefined;
+    this.head = this.head.next;
+    return n.data;
+  }
+  length() : number {
+    let n = this.head;
+    let l = 0;
+    while(n) {
+      l++;
+      n = n.next;
+    }
+    return l;
+  }
+  print():void {
+    let n = this.head;
+    while(n) {
+      console.log(n.data);
+      n = n.next;
+    }
+  }
+  
+}
